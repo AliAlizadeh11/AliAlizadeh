71a5d1244a396af25e20951a20fe441f3aac590d
AliAlizadeh11 <alializadeh.dev11@gmail.com>
2025-07-29 12:58:18 +0330

    fix: solve issue in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,9 +6,10 @@ const Hero: React.FC = () => {
     return (
         <section className="w-full max-w-6xl flex flex-col md:flex-row items-center justify-between py-12 sm:py-16 md:py-20 px-4 sm:px-6 lg:px-8 bg-transparent mb-10">
             <div className="w-full md:w-1/2 text-center md:text-left mb-8 md:mb-0 md:pr-8 lg:pr-12">
-                <p className="text-lg sm:text-xl dark:text-gray-400 text-gray-600 mb-2">Hi, my name is</p>
-                <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold dark:text-white text-gray-900 mb-4">Ali Alizadeh</h1>
-                <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold dark:text-gray-300 text-gray-700 mb-6 md:mb-8">Computer Engineering Student</h2>
+                <p className="text-base sm:text-lg dark:text-gray-400 text-gray-600 mb-2">Hi, my name is</p>
+                <h1 className="text-2xl sm:text-6xl md:text-4xl lg:text-5xl font-bold dark:text-white text-gray-900 mb-2">Ali Alizadeh</h1>
+                <h2 className="text-lg sm:text-xl md:text-2xl font-semibold dark:text-gray-300 text-gray-700 mb-2">Computer Engineering Student</h2>
+                <h3 className="text-sm sm:text-base md:text-lg font-medium dark:text-gray-400 text-gray-700 mb-6 md:mb-8">Machine Learning & Computational Biology Enthusiast</h3>
 
                 <div className="flex flex-col sm:flex-row items-center justify-center md:justify-start space-y-4 sm:space-y-0 sm:space-x-4">
                     <a href={require('../assets/AliAlizadeh-CV.pdf')} target="_blank" rel="noopener noreferrer" className="w-full sm:w-auto px-6 py-3 border border-gray-500 dark:text-white text-gray-800 rounded-lg hover:bg-gray-700 dark:hover:bg-gray-700 hover:text-white dark:hover:text-white transition duration-300 ease-in-out text-center">
